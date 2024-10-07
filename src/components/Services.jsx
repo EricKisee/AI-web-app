49a3de686d84b2328c118f529dd0c022c0fb3e33
erickisee <erickiseemulwa@gmail.com>
2024-10-07 16:47:29 +0300

    services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import Heading from "./Heading"
 import Section from "./Section"
 import {service1, service2, service3, check} from '../assets'
+import {brainwaveServices, brainwaveServicesIcons} from '../constants'
 
 const Services = () => {
   return (
@@ -10,8 +11,18 @@ const Services = () => {
 
             <div className="relative">
                 <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
-                    <div>
-                        <img src={service1} />
+                    <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+                        <img className="w-full h-full object-cover md:object-right" width={800} height={730} alt="Smartest AI" src={service1} />
+                    </div>
+                    <div className="relative z-1 max-w-[17rem] ml-auto"> 
+                        <h4 className="h4 mb-4">Smartest AI</h4>
+                        <p className="body-2 mb-[3rem]">Brainwave unloacks the potential of AI-Powered Applications</p>
+                        <ul className="body-2 mb-[3rem] text-n-3">{brainwaveServices.map((item, index)=>(
+                            <li key={index} className="flex items-start py-4 border-t border-n-6">
+                                <img src={check} alt="check" width={24} height={24}/>
+                                <p className="ml-4">{item}</p>
+                            </li>
+                        ))}</ul>
                     </div>
                 </div>
             </div>
